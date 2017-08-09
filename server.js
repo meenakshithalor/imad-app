d54b0cdbdf4d41502e15c233bce3907e1c028644
meenakshithalor <30684881+meenakshithalor@users.noreply.github.com>
2017-08-09 13:14:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Article-two");
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("Article-three");
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
